c36efc419f744b9eee3910de2a4f3753197ce647
Revengeslayer <gccbs05591gw2@gmail.com>
2022-01-12 23:59:17 +0800

    09 1/12 2358

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -18,6 +18,10 @@ public class Main : MonoBehaviour
     public float timeBeforeNextJump = 1.2f;
     private float canJump = 0f;
 
+    private float firstClickTime;
+    private float secClickTime;
+    private float doubleSpacing=0.2f;
+    
     private void Awake()
     {
         Terrain();
@@ -33,53 +37,16 @@ public class Main : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Bye();
+
+
         bool isAttack=playerAnimator.GetBool("isAttack");
         bool isJump = playerAnimator.GetBool("isJump");
+        Move(isAttack);
         if (Input.GetKeyDown(KeyCode.Z) && !isAttack && !isJump)
         {
             playerAnimator.SetTrigger("Attack");          
-        }
-
-
-        if (Input.GetKey(KeyCode.UpArrow)&&!isAttack)
-        {
-            playerAnimator.SetBool("isWalk", true);
-            player.transform.position += player.transform.forward * Time.deltaTime * speed;         
-        }
-        else 
-        {
-            playerAnimator.SetBool("isWalk", false);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) && !isAttack)
-        {
-            playerAnimator.SetBool("isWalkL", true);
-            player.transform.Rotate(0, -100 * Time.deltaTime, 0);
-            player.transform.position += player.transform.forward * Time.deltaTime / 10* speed;
-
-        }
-        else
-        {
-            playerAnimator.SetBool("isWalkL", false);
-        }
-        if (Input.GetKey(KeyCode.DownArrow) && !isAttack)
-        {
-            playerAnimator.SetBool("isWalkB", true);
-            player.transform.position -= player.transform.forward * Time.deltaTime* speed;
-        }
-        else
-        {
-            playerAnimator.SetBool("isWalkB", false);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) && !isAttack)
-        {
-            playerAnimator.SetBool("isWalkR", true);
-            player.transform.Rotate(0, 100 * Time.deltaTime, 0);
-            player.transform.position += player.transform.forward * Time.deltaTime / 10* speed;
-        }
-        else
-        {
-            playerAnimator.SetBool("isWalkR", false);
-        }
+        }       
         if (Input.GetButtonDown("Jump") && Time.time > canJump && !isAttack)
         {
             playerAnimator.SetTrigger("Jump");
@@ -87,6 +54,84 @@ public class Main : MonoBehaviour
             canJump = Time.time + timeBeforeNextJump;
         }
 
+    }
+
+    void Move(bool isAttack)
+    {
+
+        //方向       上
+        if (Input.GetKey(KeyCode.UpArrow) && !isAttack)
+        {
+            playerAnimator.SetTrigger("Walk");
+            playerAnimator.SetBool("isWalkF", true);
+            player.transform.position += player.transform.forward * Time.deltaTime * speed;
+        }
+        else
+        {
+            playerAnimator.SetBool("isWalkF", false);
+        }
+        //方向       下
+        if (Input.GetKey(KeyCode.DownArrow) && !isAttack)
+        {
+            playerAnimator.SetTrigger("Walk");
+            playerAnimator.SetBool("isWalkB", true);
+            player.transform.position -= player.transform.forward * Time.deltaTime * speed;
+        }
+        else
+        {
+            playerAnimator.SetBool("isWalkB", false);
+        }
+        //方向       左
+        if (Input.GetKey(KeyCode.LeftArrow) && !isAttack)
+        {
+            playerAnimator.SetTrigger("Walk");
+            playerAnimator.SetBool("isWalkL", true);
+            player.transform.Rotate(0, -100 * Time.deltaTime, 0);
+            player.transform.position += player.transform.forward * Time.deltaTime / 10 * speed;
+
+        }
+        else
+        {
+            playerAnimator.SetBool("isWalkL", false);
+        }
+        //方向       右
+        if (Input.GetKey(KeyCode.RightArrow) && !isAttack)
+        {
+            playerAnimator.SetTrigger("Walk");
+            playerAnimator.SetBool("isWalkR", true);
+            player.transform.Rotate(0, 100 * Time.deltaTime, 0);
+            player.transform.position += player.transform.forward * Time.deltaTime / 10 * speed;
+        }
+        else
+        {
+            playerAnimator.SetBool("isWalkR", false);
+        }
+    }
+
+    void Bye()
+    {
+        if (Input.GetKey(KeyCode.B))
+        {
+            secClickTime = Time.time - firstClickTime;
+            Debug.Log(secClickTime);
+            if (secClickTime< doubleSpacing)
+            {
+                playerAnimator.SetBool("isRun",true);
+            }
+            else
+            {
+                playerAnimator.SetBool("isBye", true);
+            }         
+            firstClickTime = Time.time;
+        }
+        else
+        {
+            playerAnimator.SetBool("isBye", false);
+        }
+        if (Input.GetKeyUp(KeyCode.B) && playerAnimator.GetBool("isRun"))
+        {
+            playerAnimator.SetBool("isRun", false);
+        }
     }
 
     void Mobs()
